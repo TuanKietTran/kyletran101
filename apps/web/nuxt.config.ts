96853b1ac4d8b5eb04fd5bd05386b5fb919ce19c
TuanKietTran <kyletran101.work@gmail.com>
2025-01-05 17:47:25 +0700

    Add Post content type

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/strapi"],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+  },
 });
